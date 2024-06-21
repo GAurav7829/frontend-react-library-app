d81cd5352b14263b7a41060efa67e4e807172d6b
Gaurav Singh <grvsgh7@gmail.com>
2024-06-21 20:05:22 +0530

    Add Search Books Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { NavBar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 
 export const App = () => {
   return (
     <>
       <NavBar />
-      <HomePage />
+      {/*<HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </>
   );
